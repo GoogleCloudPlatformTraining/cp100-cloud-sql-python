0316ab912cff7cd70f52159b54e5984f61eeb749
Sharif Salah <sharif.salah@gmail.com>
2015-03-17 20:52:16 +0000

    Initial draft publish

---
+++ app/app.py
@@ -0,0 +1,39 @@
+import os
+import MySQLdb
+
+from flask import Flask
+from flask import request, redirect, render_template, url_for
+from flask import Response
+
+app = Flask(__name__)
+
+IP_ADDRESS = os.environ['CLOUDSQL_IP']
+PASSWORD = os.environ['CLOUDSQL_PWD'] # or os.environ.get('')
+
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
+    if request.method == 'POST':
+
+        return redirect(url_for('main_page'))
+    else:
+        db=MySQLdb.connect(host=IP_ADDRESS, port=3306, db='guestbook', user='root', passwd=PASSWORD)
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM entries')
+
+        entries = [];
+        for row in cursor.fetchall():
+            entries.append(dict(('entry',cgi.escape(row[0]))))
+
+        variables = {'entries': entries}
+        template = JINJA_ENVIRONMENT.get_template('main.html')
+        self.response.write(template.render(variables))
+        db.close()
+        return render_template('main.html', entries=entries)
+
+#@app.route('/clear', methods=['POST'])
+#def clear_entries():
+#    ..
+#    return redirect(url_for('main_page'))
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=80)
