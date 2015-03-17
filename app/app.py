89b8a68f157a84dd06e96da50ec4fddefbb93179
Sharif Salah <sharif.salah@gmail.com>
2015-03-17 23:16:45 +0000

    Implemented a clear post method

--- app/app.py
+++ app/app.py
@@ -8,11 +8,11 @@ from flask import Response
 app = Flask(__name__)
 
 IP_ADDRESS = os.environ['CLOUDSQL_IP']
-PASSWORD = os.environ['CLOUDSQL_PWD'] # or os.environ.get('')
+PASSWORD = os.environ['CLOUDSQL_PWD']
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
-    db=MySQLdb.connect(host=IP_ADDRESS, port=3306, db='guestbook', user='root', passwd=PASSWORD)
+    db = MySQLdb.connect(host=IP_ADDRESS, port=3306, db='guestbook', user='root', passwd=PASSWORD)
     cursor = db.cursor()
     if request.method == 'POST':
         cursor.execute('INSERT INTO entries (entry) VALUES( ? )', [request.form['entry'],])
@@ -21,14 +21,18 @@ def main_page():
         return redirect(url_for('main_page'))
     else:
         cursor.execute('SELECT * FROM entries')
-        entries = [dict(entry=row[0]) for row in cursor.fetchall()]
+        entries = [dict(entry=row[1]) for row in cursor.fetchall()]
         db.close()
         return render_template('main.html', entries=entries)
 
-#@app.route('/clear', methods=['POST'])
-#def clear_entries():
-#    ..
-#    return redirect(url_for('main_page'))
+@app.route('/clear', methods=['POST'])
+def clear_entries():
+    db = MySQLdb.connect(host=IP_ADDRESS, port=3306, db='guestbook', user='root', passwd=PASSWORD)
+    cursor = db.cursor()
+    cursor.execute('DELETE from entries')
+    db.commit()
+    db.close()
+    return redirect(url_for('main_page'))
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80)
